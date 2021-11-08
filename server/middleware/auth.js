224c0753d091f9a36e20fd2e8f4760733c0532eb
Kyungbae Min <kbmin1129@gmail.com>
2021-11-08 16:40:05 +0900

    auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+const User = require("../models/User");
+const { wrapAsync } = require("../utils/helper");
+
+module.exports.isLoggedIn = (req, res, next) => {
+	if (!req.session.userId) {
+		throw new Error("Need to login first");
+	}
+	next();
+};
+
+module.exports.isAgent = wrapAsync(async (req, res, next) => {
+	const id = req.params.id;
+	const user = await User.findById(id);
+	if (user.agent && !user.agent.equals(req.session.userId)) {
+		throw new ExpressError("Not an authorized agent for this user", 401);
+	}
+	next();
+});
