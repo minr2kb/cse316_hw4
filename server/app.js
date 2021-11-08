6bf5b7f13d7903d1b523aa8db2d9be66503b1e5a
Kyungbae Min <kbmin1129@gmail.com>
2021-11-08 16:32:52 +0900

    added session

--- server/app.js
+++ server/app.js
@@ -1,18 +1,55 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const Note = require("./models/Note");
+// const User = require("./models/User");
+
+// const app = express();
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+
 const express = require("express");
-const mongoose = require("mongoose");
-const Note = require("./models/Note");
-const User = require("./models/User");
+const libraryRoutes = require("./routes/notes");
+const userRoutes = require("./routes/users");
 
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-// var mongoDB = "mongodb://localhost:27017/MyNote";
-var mongoDB =
-	"mongodb+srv://admin:admin@mycluster.lr4aw.mongodb.net/MyNote?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo"); // MongoDB session store
+
+var dbURL = process.env.MONGO_URL || "mongodb://localhost:27017/MyNote";
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+const sessionSecret = "KyungbaeMin";
+
+const store = MongoStore.create({
+	mongoUrl: dbURL,
+	secret: sessionSecret,
+	touchAfter: 24 * 60 * 60,
+});
+
+// Changing this setting to avoid a Mongoose deprecation warning:
+mongoose.set("useFindAndModify", false);
+
+const sessionConfig = {
+	store,
+	name: "session",
+	secret: sessionSecret,
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+		// secure: true,
+	},
+};
+
+app.use(session(sessionConfig));
 
 function wrapAsync(fn) {
 	return function (req, res, next) {
